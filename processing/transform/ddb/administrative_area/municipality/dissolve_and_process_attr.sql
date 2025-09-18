2dd8ed509933090b868d71f4fe2c3d98bd735cf7
K-Oxon <ko1011qfp@gmail.com>
2025-09-18 17:10:55 +0900

    fix sql

--- processing/transform/ddb/administrative_area/municipality/dissolve_and_process_attr.sql
+++ processing/transform/ddb/administrative_area/municipality/dissolve_and_process_attr.sql
@@ -9,32 +9,34 @@ create or replace table dwh.dwh.japan_administrative_area_1741 as (
         n03_003 as county_name,
         n03_004 as city_name,
         n03_005 as ward_name,
-        n03_007 as ld_code_5,
+        n03_007 as lg_code_5,
         geom,
         from raw_area
-      where n03_003 != '所属未定地' -- 所属未定地を除く
+      where n03_004 != '所属未定地' -- 所属未定地を除く
     ),
     join_mst_city as (
       select mst_city.municipality_code as lg_code,
-        left(mst_city.municipality_code, 5) as ld_code_5,
+        left(mst_city.municipality_code, 5) as lg_code_5,
         left(mst_city.municipality_code, 2) as prefecture_code,
         renamed.prefecture_name,
         renamed.county_name,
         renamed.city_name,
         renamed.ward_name,
+        mst_city.is_hoppo_city,
         renamed.geom
       from renamed
-        left outer join dwh.dwh.mst_city as mst_city on renamed.ld_code_5 = mst_city.lg_code_5
+        left outer join dwh.dwh.mst_city as mst_city on renamed.lg_code_5 = mst_city.lg_code_5
     ),
     -- 2) 同一市区町村内のマルチパーツを Union
     dissolve_multi_polygon as (
       select lg_code,
-        ld_code_5,
+        lg_code_5,
         prefecture_code,
         prefecture_name,
         county_name,
         city_name,
         ward_name,
+        is_hoppo_city,
         st_union_agg (geom) as geom
       from join_mst_city
       group by all
@@ -42,23 +44,24 @@ create or replace table dwh.dwh.japan_administrative_area_1741 as (
     -- 3) 市政令指定都市を統合
     dissolve_city as (
       select lg_code,
-        ld_code_5,
+        lg_code_5,
         prefecture_code,
         prefecture_name,
         county_name,
         city_name,
-        -- 雑な処理(実際はマスタと結合させるなど)
+        is_hoppo_city,
         st_coverageunion_agg (geom) as geom -- 隣接前提
       from dissolve_multi_polygon
       group by all
     ),
     final as (
       select lg_code,
-        ld_code_5,
+        lg_code_5,
         prefecture_code,
         prefecture_name,
         county_name,
         city_name,
+        is_hoppo_city,
         st_makevalid (geom) as geom
       from dissolve_city
     )
@@ -68,11 +71,12 @@ create or replace table dwh.dwh.japan_administrative_area_1741 as (
 -- GeoJSON FeatureCollectionとしてファイルに出力する
 COPY (
   SELECT lg_code,
-    ld_code_5,
+    lg_code_5,
     prefecture_code,
     prefecture_name,
     county_name,
     city_name,
+    is_hoppo_city,
     geom
   FROM dwh.dwh.japan_administrative_area_1741
   order by lg_code
